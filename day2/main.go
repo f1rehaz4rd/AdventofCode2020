6413fd1a113bf7c3bd1ec3deefa0903dba8dc714
f1rehaz4rd <wcjon123@gmail.com>
2020-12-02 22:52:18 -0800

    Day 2 - Part 2

--- day2/main.go
+++ day2/main.go
@@ -9,6 +9,62 @@ import (
 	"strings"
 )
 
+// ChallengeTwoValidate validates based of the second challenge's
+// parameters for the password policy
+func ChallengeTwoValidate(entry string) bool {
+
+	valid := false
+
+	//
+	// Parse the entry to get values needed
+	//
+	tmpSplit := strings.Split(entry, ":")
+
+	pos1, _ := strconv.Atoi(strings.Split(tmpSplit[0], "-")[0])
+	pos2, _ := strconv.Atoi(strings.Split(strings.Split(tmpSplit[0], "-")[1], " ")[0])
+
+	letter := strings.Split(tmpSplit[0], " ")[1]
+	password := strings.TrimSpace(tmpSplit[1])
+
+	//
+	// Do validation
+	//
+
+	cmp1 := string(password[pos1-1])
+	cmp2 := string(password[pos2-1])
+
+	if cmp1 == letter && cmp2 != letter {
+		valid = true
+	} else if cmp1 != letter && cmp2 == letter {
+		valid = true
+	}
+
+	// Return if it was valid
+	return valid
+}
+
+// ChallengeTwo Solves and prints the answer to part 1
+func ChallengeTwo() {
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal("failed to open file")
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	ctr := 0
+
+	for scanner.Scan() {
+		if ChallengeTwoValidate(scanner.Text()) {
+			ctr++
+		}
+	}
+
+	fmt.Printf("# of valid passwords: %d\n", ctr)
+}
+
 // ChallengeOneValidate validates based of the first challenge's
 // parameters for the password policy
 func ChallengeOneValidate(entry string) bool {
@@ -62,5 +118,6 @@ func ChallengeOne() {
 }
 
 func main() {
-	ChallengeOne()
+	// ChallengeOne()
+	ChallengeTwo()
 }
