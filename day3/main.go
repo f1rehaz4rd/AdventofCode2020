e249268cd8cfcfd15f422d39c64054f17aea1808
f1rehaz4rd <wcjon123@gmail.com>
2020-12-03 21:04:50 -0800

    Day 3 - Part 2

--- day3/main.go
+++ day3/main.go
@@ -11,6 +11,8 @@ const FILENAME = "input.txt"
 const TREECHAR = "#"
 const RIGHTMOVEVAL = 3
 
+var RIGHTMOVEVALS = []int{1, 3, 5, 7}
+
 func buildBoard(inputFile string) ([][]string, int, int) {
 
 	board := [][]string{}
@@ -40,6 +42,50 @@ func buildBoard(inputFile string) ([][]string, int, int) {
 	return board, row, col
 }
 
+func ChallengeTwo() {
+	board, row, col := buildBoard(FILENAME)
+
+	ans := 1
+	trees := []int{0, 0, 0, 0, 0}
+
+	rightMoves := []int{1, 3, 5, 7}
+	downTwoCheck := 1
+
+	for i := 1; i < row; i++ {
+		//
+		// Check the single down moves
+		//
+		for j := 0; j < len(rightMoves); j++ {
+			if board[i][rightMoves[j]] == TREECHAR {
+				trees[j]++
+			}
+
+			rightMoves[j] = (rightMoves[j] + RIGHTMOVEVALS[j]) % col
+		}
+
+		//
+		// Check the down 2
+		//
+		if (i % 2) == 0 {
+			if board[i][downTwoCheck] == TREECHAR {
+				trees[4]++
+			}
+
+			downTwoCheck = (downTwoCheck + 1) % col
+		}
+	}
+
+	//
+	// Multiply them together
+	//
+	for i := 0; i < len(trees); i++ {
+		ans = ans * trees[i]
+	}
+
+	// Print Answer
+	fmt.Printf("ans: %d\n", ans)
+}
+
 func ChallengeOne() {
 
 	board, row, col := buildBoard(FILENAME)
@@ -61,5 +107,6 @@ func ChallengeOne() {
 }
 
 func main() {
-	ChallengeOne()
+	// ChallengeOne()
+	ChallengeTwo()
 }
